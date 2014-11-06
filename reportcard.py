dc357a776a1043110f17eeec8405c15b343d5817
Noah Kim <noahbkim@gmail.com>
2014-11-05 19:57:45 -0500

    Super update
    New GUI and better code organization

--- reportcard.py
+++ reportcard.py
@@ -7,80 +7,87 @@ import tkinter.ttk
 import tkinter.scrolledtext
 import tkinter.messagebox
 import re
+import os
+
+import threading
+
 import requests
 import bs4
 
 # Constant
-URL = "http://www.edline.net"
+MAIN_URL = "http://www.edline.net"
 LOGIN_URL = "http://www.edline.net/InterstitialLogin.page"
 LOGIN_POST_URL = "https://www.edline.net/post/InterstitialLogin.page"
+NOTIFICATIONS_URL = "/Notifications.page"
+REPORTS_URL = "/Current_Assignments_Report"
 
-# Function
-def shorten(string, length):
-    """If a string is too long truncate and add ellipsis."""
-    return string[:length-3] + "..." if len(string) > length else string
+HEADER_DATA = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko)"
+                  "Chrome/31.0.1650.63"
+                  "Safari/537.36"
+}
 
-# Class
+LOGIN_DATA = {
+    "TCNK": "authenticationEntryComponent",
+    "submitEvent": "1",
+    "guestLoginEvent": "",
+    "enterClicked": "true",
+    "bscf": "",
+    "bscv": "",
+    "targetEntid": "",
+    "ajaxSupported": "yes"
+}
+
+# Edline
 class Scraper:
     """Automated Edline scraper."""
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko)"
-                      "Chrome/31.0.1650.63"
-                      "Safari/537.36"
-    }
-
-    data = {
-        "TCNK": "authenticationEntryComponent",
-        "submitEvent": "1",
-        "guestLoginEvent": "",
-        "enterClicked": "true",
-        "bscf": "",
-        "bscv": "",
-        "targetEntid": "",
-        "ajaxSupported": "yes"
-    }
-
     def __init__(self, username, password, pages):
         """Initialize a new scraper."""
-        self.data["screenName"] = username
-        self.data["kclq"] = password
+        self.username = username
+        self.password = password
         self.pages = pages
 
-    def log_in(self):
+        self.finished = False
+
+    def login(self):
         """Log in to Edline."""
-        response = requests.get(LOGIN_URL, headers=self.headers)
+        response = requests.get(LOGIN_URL, headers=HEADER_DATA)
         if response.status_code != 200:
             tkinter.messagebox.showerror("Failed to access Edline!")
-            quit()
+            return
         self.cookies = response.cookies
-        response = requests.post(LOGIN_POST_URL, data=self.data,
-                                 headers=self.headers, cookies=self.cookies,
+
+        login_data = LOGIN_DATA.copy()
+        login_data.update({"screenName": self.username, "kclq": self.password})
+        response = requests.post(LOGIN_POST_URL, login_data,
+                                 headers=HEADER_DATA, cookies=self.cookies,
                                  allow_redirects=False)
-        location_url = response.headers["location"]
-        if location_url == URL + "/Notification.page":
-            tkinter.messagebox.showerror("Bad credentials")
-            quit()
+
+        self.location = response.headers["location"]
+        if self.location == MAIN_URL + NOTIFICATIONS_URL:
+            tkinter.messagebox.showerror("Bad login credentials!")
+            return
+
         self.cookies.update(response.cookies)
 
-    def scrape_reports(self):
-        """Scrape reports from Edline"""
+    def scrape(self):
+        """Scrape reports off of Edline."""
         self.reports = []
         for page in self.pages:
-            reports_url = page + "/Current_Assignments_Report"
-            response = requests.get(reports_url, headers=self.headers,
+            reports_url = page + REPORTS_URL
+            response = requests.get(reports_url, headers=HEADER_DATA,
                                     cookies=self.cookies)
-            soup = bs4.BeautifulSoup(response.text)
-            iframe = soup.findChild("iframe", {"id": "docViewBodyFrame"})
-            reports_actual_url = iframe["src"]
             
-            response = requests.get(reports_actual_url, headers=self.headers,
-                                    cookies=self.cookies)
-            course = re.search(r"Class: ([^(]+) \(\w+\)", response.text)
-            course = course.groups()[0]
             soup = bs4.BeautifulSoup(response.text)
+            source = soup.findChild("iframe", {"id": "docViewBodyFrame"})["src"]
+            response = requests.get(source, headers=HEADER_DATA,
+                                    cookies=self.cookies)
+            pattern = re.compile(r"Class: ([^(]+) \(\w+\)")
+            course = pattern.search(response.text).groups()[0]
 
+            soup = bs4.BeautifulSoup(response.text)
             data = []
             for child in soup.findChildren("td", {"valign": "center"}):
                 text = child.get_text()       
@@ -91,141 +98,105 @@ class Scraper:
             report = Report(course, data)
             self.reports.append(report)
 
+        self.finished = True
+
 class Report:
-    """Generated report based on Edline data."""
+    """Report container class for Edline data."""
 
     def __init__(self, course, data):
         """Initialize a new report."""
         self.course = course
         self.data = data
+        self.parse()
 
-        self.parse_data()
-
-    def parse_data(self):
+    def parse(self):
         self.categories = []
-        for i in range(0, len(self.data) - 3, 5):
-            self.categories.append((self.data[i], self.data[i+1],
-                                    self.data[i+3], self.data[i+2]))
-        self.cumulative = (self.data[-2], self.data[-1])
+        for i in range(0, len(self.data)-3, 5):
+            self.categories.append({
+                "name": self.data[i],
+                "weight": self.data[i+1],
+                "percentage": self.data[i+3],
+                "fraction": self.data[i+2]
+            })
+        self.cumulative = {"percentage": self.data[-2], "letter": self.data[-1]}
 
-class Viewer:
-    """Main application user interface."""
+class Application:
+    """Main application framework and user interface."""
 
-    def __init__(self):
-        """Initialize a new reportcard viewer."""
-        pass
-
-    def load(self):
-        """Load the application."""        
+    def setup(self):
+        """Set up application scraper and interface data."""
         with open("edline.txt") as file:
             contents = file.read()
-            username, password, *pages = [line.strip() for line in contents.split("\n")]
-        self.progressbar.step(10) # 10
-        self.window.update()
+            lines = [line.strip() for line in contents.split(os.linesep)]
+            username, password, *pages = lines
 
         self.scraper = Scraper(username, password, pages)
-        self.progressbar.step(10) # 20
-        self.window.update()
-    
-        self.scraper.log_in()
-        self.progressbar.step(15) # 35
-        self.window.update()
-
-        self.scraper.scrape_reports()
-        self.progressbar.step(64) # 99
-        self.window.update()
+        self.scraper.login()
+        self.scraper.scrape()
 
         self.window.destroy()
         self.window.quit()
 
-    def setup(self):
-        """Set up for Edline data."""#List on side
-        if self.sidebar_list.size() == 0:
-            self.sidebar_list.insert("end", "Summary")
-            for report in self.scraper.reports:
-                self.sidebar_list.insert("end", report.course.title())
-        self.sidebar_list.selection_set(0)
-
-    def poll(self):
-        """Update the current content."""
-        if self.sidebar_list.curselection():
-            selection = int(self.sidebar_list.curselection()[0])
-            if selection == 0:
-                gpa = []
-                display = ""
-                for report in self.scraper.reports:
-                    display += "%-20s%-8s%s\n" % ((report.course.title(),) +
-                                                  report.cumulative)
-                    gpa.append(['E','D','C','B','A'].index(
-                        report.cumulative[1]))
-                self.content_text.config(state="normal")
-                self.content_text.delete("1.0", "end")
-                self.content_text.insert("1.0", display)
-                self.content_text.insert("end", '\n')
-                self.content_text.insert("10.0","%-20s%-8s" %
-                                         ('GPA', str(sum(gpa) / len(gpa))))
-                self.content_text.config(state="disabled")
-            else:
-                selection -= 1
-                report = self.scraper.reports[selection]
-                display = ""
-                display += "%s\n\n" % report.course.title()
-                for category in report.categories:
-                    name = "%s (%s%%)" % (shorten(category[0], 17), category[1])
-                    display += "%-27s%-7s%s\n" % (name, category[2],
-                                                  category[3])
-                display += "\n%-27s%-7s%s\n" % (("Cumulative",) +
-                                                report.cumulative)
-                self.content_text.config(state="normal")
-                self.content_text.delete("1.0", "end")
-                self.content_text.insert("1.0", display)
-                self.content_text.config(state="disabled")               
-                    
-        self.window.after(100, self.poll)
+    def load(self):
+        """Load class data."""        
+        for report in self.scraper.reports:
+            self.treeview.insert(
+                "", "end", report.course,
+                values=(
+                    " "*3+report.course.title(),
+                    report.cumulative["percentage"],
+                    report.cumulative["letter"]
+                ))
+            for category in report.categories:
+                self.treeview.insert(
+                    report.course, "end", report.course+"/"+category["name"],
+                    values=(
+                        " "*5+category["name"]+" ("+category["weight"]+"%)",
+                        " "*2+category["percentage"],
+                        " "*2+category["fraction"],
+                    ))
+                                                        
 
     def build(self):
-        """Build the interface."""
+        """Build the application interface."""
         self.window = tkinter.Tk()
-        self.window.title("Report Card")
-        self.window.config(width=500)
+        self.window.title("Report card")
+        self.window.protocol("WM_DELETE_WINDOW", int)
 
         self.content = tkinter.Frame(self.window)
         self.content.pack(fill="both", padx=10, pady=5)
         self.message = tkinter.Label(self.content)
-        self.message.config(text="Loading...")
-        self.message.pack(pady=5)
+        self.message.config(text="Loading...", anchor="w", width=30)
+        self.message.pack(pady=5, fill="x")
         self.progressbar = tkinter.ttk.Progressbar(self.content)
-        self.progressbar.pack(side="bottom", pady=5)
+        self.progressbar.config(mode="indeterminate")
+        self.progressbar.pack(fill="x", pady=5)
 
-        self.window.after(15, self.load)
+        setup_thread = threading.Thread(target=self.setup)
+        setup_thread.start()
+
         self.window.mainloop()
+
 
         self.window = tkinter.Tk()
-        self.window.title("Report Card")
+        self.window.title("Report card")
 
-        self.sidebar = tkinter.Frame(self.window)
-        self.sidebar.pack(side="left", fill="y")
-        self.sidebar_list = tkinter.Listbox(self.sidebar)
-        self.sidebar_list.config(width=25, selectmode="single")
-        self.sidebar_list.pack(side="left", fill="y")
-        self.sidebar_scroll = tkinter.Scrollbar(self.sidebar)
-        self.sidebar_scroll.pack(side="right", fill="y")
-        self.sidebar_list.config(yscrollcommand=self.sidebar_scroll.set)
-        self.sidebar_scroll.config(command=self.sidebar_list.yview)
-        
-        self.content = tkinter.Frame(self.window)
-        self.content.pack(side="right", fill="y")
-        self.content_text = tkinter.scrolledtext.ScrolledText(self.content)
-        self.content_text.config(width=50, state="disabled")
-        self.content_text.pack(fill="both")
-
-        self.window.after(10, self.setup)
-        self.window.after(100, self.poll)
+        self.treeview = tkinter.ttk.Treeview(self.window)
+        self.treeview.config(columns=("Course", "Percentage", "Grade"))
+        self.treeview.heading("#1", text="Course", anchor="w")
+        self.treeview.heading("#2", text="Percentage", anchor="w")
+        self.treeview.heading("#3", text="Grade", anchor="w")
+        self.treeview.column('#0', stretch="no", minwidth=0, width=0)
+        self.treeview.column('#1', stretch="no", minwidth=200, width=200)
+        self.treeview.column('#2', stretch="no", minwidth=100, width=100)
+        self.treeview.column('#3', stretch="no", minwidth=90, width=90)
+        self.treeview.pack(fill="both", expand=1, padx=10, pady=10)
 
     def main(self):
-        """Run the interface."""
+        """Run the application."""
         self.window.mainloop()
 
-v = Viewer()
-v.build()
-v.main()
+app = Application()
+app.build()
+app.load()
+app.main()
